e32b4d987f615c9b6e92848d39c58018e9dd7660
Eleonore <e.duvelle@dartmouth.edu>
2022-06-11 15:09:47 -0400

    Now using os.path.join instead of addition for all filenames
    that should make it able to work on Windows and Linux as well..

--- ScriptMS4.py
+++ ScriptMS4.py
@@ -3,6 +3,7 @@
 Created on Wed Jun  8 10:40:11 2022
 
 @author: kayva
+modified by ED on 2022-06-11
 """
 
 import os
@@ -20,37 +21,60 @@ import numpy as np
  
 from spikeinterface.exporters import export_to_phy
 
+
 def creatparam(direc): ##Function that will create the parameters and geometry file needed for mountainsort
     import json
 
     parameter = {"samplerate": 30000, "spike_sign": -1}
     geom = np.array([[1, 0],[2, 0],[3, 0],[4, 0]])
-    np.savetxt(direc+'geom.csv', geom, delimiter=',')
-
-    with open(direc+'params.json', 'w') as mon_fichier:
+    #ED mod file name handling
+    np.savetxt(os.path.join(direc,'geom.csv'), geom, delimiter=',')
+    # ED mod file name handling
+    this_file_path = os.path.join(direc,'params.json')
+    with open(this_file_path, 'w') as mon_fichier:
     	json.dump(parameter, mon_fichier)
+        
+    print('Should have saved param file to:',this_file_path )
 
-def run_Mountainsort(recording,directory_output): ##Function that will run mountainsort, extract the information from mountainsort and export to phy
+def run_Mountainsort(recording, directory_output): ##Function that will run mountainsort, extract the information from mountainsort and export to phy
     ss.installed_sorters()
     default_MS = ss.Mountainsort4Sorter.default_params()
     print(default_MS)
-    sorting_MS = ss.run_mountainsort4(recording, output_folder=directory_output+'/results_MS', verbose=True, **default_MS,)
+    this_output_folder = os.path.join(directory_output, 'results_MS')
+    sorting_MS = ss.run_mountainsort4(recording,
+                                      output_folder = this_output_folder,
+                                      verbose = True, **default_MS,)
         
-    we_all = si.extract_waveforms(recording, sorting_MS, folder=directory_output+"/wf_MS", 
-                                      max_spikes_per_unit=None, progress_bar=True)
-    
-    export_to_phy(we_all, output_folder=directory_output+'/phy_MS',
-                      progress_bar=True, total_memory='100M')
+    #ED added use of os.path.join
+    this_output_folder = os.path.join(directory_output,'wf_MS')
+    we_all = si.extract_waveforms(recording, sorting_MS, folder = this_output_folder, 
+                                      max_spikes_per_unit = None, progress_bar = True)
+    #ED added use of os.path.join
+    this_output_folder = os.path.join(directory_output,'phy_MS')
+    export_to_phy(we_all, output_folder = this_output_folder,
+                      progress_bar = True, total_memory = '100M')
 
+if __name__ == '__main__':
+    #ED: the 'input' command makes Spyder crash. So we will input the files 
+    # directly as variables here instead for now.
+    # directory = input("Enter the data directory: ")
+    # name = input("Enter the name of the file to spikesort: ")
+    dir_test = r"C:\Users\Eleonore\Documents\GitHub\SpikeinterfaceMS4_GenzelLab"
+    # dir_test = os.sep.join([dir_test])
+    directory = r"\\dartfs.dartmouth.edu\rc\lab\D\DuvelleE\mvdmlab_ED_fs\Spike_sorting\Mallory_sample_data\tetrode_recording.mountainsort"
+    if not os.path.isdir(directory):
+        print('Directory not found!')
+    name = 'tetrode_recording.nt1.mda'
+    print(directory)
+    output = os.path.join(directory, 'output')
+    # output = directory+'\output' # Note the backslash will change between linux and win
+    # ED
+    # directory = str(directory)
+    creatparam(directory) #Create the parameter and geom file
 
-directory = input("Enter the directory: ")
-name = input("Enter the name of the file to spikesort: ")
-print(directory)
-output = directory+'/output'
-creatparam(directory) #Create the parameter and geom file
-rec = se.MdaRecordingExtractor(directory,raw_fname=name,params_fname='params.json',geom_fname='geom.csv')
-w = sw.plot_timeseries(rec) #plot the first second of the recording
-recording_f = st.bandpass_filter(rec, freq_min=300, freq_max=6000) #Band pass filtering
-w = sw.plot_timeseries(recording_f)
-rec.annotate(is_filtered=True)
-run_Mountainsort(recording_f,output) ## Run mountainsort and export to phy
+    rec = se.MdaRecordingExtractor(directory,raw_fname=name,params_fname='params.json',geom_fname='geom.csv')
+    w = sw.plot_timeseries(rec) #plot the first second of the recording
+    recording_f = st.bandpass_filter(rec, freq_min=300, freq_max=6000) #Band pass filtering
+    w = sw.plot_timeseries(recording_f)
+    rec.annotate(is_filtered=True)
+    run_Mountainsort(recording_f,output) ## Run mountainsort and export to phy
